728d65ac85cc298b5c398c2771b70b92ced6e9f3
shatayu <shatayukulkarni@gmail.com>
2021-12-25 16:54:51 -0800

    added status and opponent team

--- pokemon/poke_env_new_copg.py
+++ pokemon/poke_env_new_copg.py
@@ -73,22 +73,52 @@ if not os.path.exists(directory):
     os.makedirs(directory)
 writer = SummaryWriter('../' + folder_location + experiment_name + 'data')
 
+def one_hot_encode_status(status, fill_null_values):
+    array = [0] * len(Status)
+    statuses = [s for s in Status]
+    
+    if status in statuses and not fill_null_values:
+        index = statuses.index(status)
+        array[index] = 1
+        
+    return array
+    
+def encode_pokemon(p, fill_null_values=False):
+    return [1 if p.active else 0, p.current_hp_fraction] + one_hot_encode_status(p.status, fill_null_values)
+
+def get_team_encoding(team):
+    pokemon_object_list = list(team.values())
+    pokemon_object_list.sort(key=lambda x: x.species)
+
+    return sum([encode_pokemon(p) for p in pokemon_object_list], [])
+
+# fills in unrevealed Pokemon from opponent with Pokemon from the agent's team
+def get_opponent_team_encoding(opponent_team, agent_team):
+    agent_pokemon_object_list = list(agent_team.values())
+    agent_pokemon_object_list.sort(key=lambda x: x.species)
+
+    opponent_pokemon_object_list = list(opponent_team.values())
+
+    opponent_pokemon_revealed_species = set([p.species for p in opponent_pokemon_object_list])
+    opponent_pokemon_full_team = []
+
+    for pa in agent_pokemon_object_list:
+        if pa.species in opponent_pokemon_revealed_species:
+            opponent_pokemon_full_team.append(next(po for po in opponent_pokemon_object_list if pa.species == po.species))
+        else:
+            opponent_pokemon_full_team.append(pa)
+    
+    return sum([encode_pokemon(p, True) for p in opponent_pokemon_full_team], []) 
 
 def get_current_state(battle):
     # remember to change STATE_DIM in pokemon_constants.py
-    is_my_current_pokemon_fainted = battle.active_pokemon.status == Status.FNT
-    is_their_current_pokemon_fainted = battle.opponent_active_pokemon.status == Status.FNT
+    result = np.array(
+        [battle.turn, len(battle.available_moves), len(battle.available_switches)] + \
+            get_team_encoding(battle.team) + \
+            get_opponent_team_encoding(battle.opponent_team, battle.team)
+        )
 
-    return np.array([battle.turn,
-        len(battle.available_moves),
-        len(battle.available_switches),
-        1 if is_my_current_pokemon_fainted else 0,
-        1 if is_their_current_pokemon_fainted else 0,
-        pokemon_to_int(battle.active_pokemon),
-        battle.active_pokemon.current_hp_fraction,
-        pokemon_to_int(battle.opponent_active_pokemon),
-        battle.opponent_active_pokemon.current_hp_fraction
-    ])
+    return result
 
 class COPGGen8EnvPlayer(Gen8EnvSinglePlayer):
     def embed_battle(self, battle):
