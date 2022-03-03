9e5457ae42fe9e7fdcf76d350bdca333f46fc2e1
shatayu <shatayukulkarni@gmail.com>
2022-03-03 15:02:51 -0800

    fixed issue where test players used wrong team

--- pokemon/test_poke_env.py
+++ pokemon/test_poke_env.py
@@ -5,47 +5,7 @@ import time
 from poke_env.player.player import Player
 from poke_env.player.random_player import RandomPlayer
 
-team = """
-Garchomp
-Ability: Rough Skin
-EVs: 252 Atk / 4 SpD / 252 Spe
-Jolly Nature
-- Dragon Claw
-- Earthquake
-- Swords Dance
-- Brick Break
-"""
-
-"""
-FOCUS ON GEN 8
-env_player class notes:
-    Documentation: https://poke-env.readthedocs.io/en/stable/player.html#module-poke_env.player.env_player
-
-    (for gen 8)
-    https://github.com/hsahovic/poke-env/blob/3aa5bdb4926e2eb5a82df77df519f4fc94aca890/src/poke_env/player/env_player.py#L567
-
-    0 <= action < 4:
-        The actionth available move in battle.available_moves is executed.
-    4 <= action < 8:
-        The action - 4th available move in battle.available_moves is executed, with
-        z-move.
-    8 <= action < 12:
-        The action - 8th available move in battle.available_moves is executed, with
-        mega-evolution.
-    8 <= action < 12:
-        The action - 8th available move in battle.available_moves is executed, with
-        mega-evolution.
-    12 <= action < 16:
-        The action - 12th available move in battle.available_moves is executed,
-        while dynamaxing.
-    16 <= action < 22
-        The action - 16th available switch in battle.available_switches is executed.
-
-    If the proposed action is illegal, a random legal move is performed.
-
-battle class notes:
-    
-"""
+from pokemon_constants import TEAM
 
 class MaxDamagePlayer(Player):
     def choose_move(self, battle):
@@ -66,11 +26,11 @@ async def main():
     # We create two players.
     random_player = RandomPlayer(
         battle_format="gen8ou",
-        team=team
+        team=TEAM
     )
     max_damage_player = MaxDamagePlayer(
         battle_format="gen8ou",
-        team=team
+        team=TEAM
     )
 
     # Now, let's evaluate our player
