cca4317417ac1122b2551e8fba6780ec8d68079c
shatayu <shatayu@outlook.com>
2022-03-09 09:45:08 -0800

    added tensorboard to git repo

--- pokemon/poke_env_new_copg.py
+++ pokemon/poke_env_new_copg.py
@@ -54,6 +54,14 @@ p1 = policy(STATE_DIM, NUM_ACTIONS + 1) # support null action being the last act
 p2 = policy(STATE_DIM, NUM_ACTIONS + 1)
 q = critic(STATE_DIM)
 
+p1.load_state_dict(
+    torch.load("../tensorboard/pokemon_critic_lr_1e2_mar2/observationsmodel/agent1_4950.pth"))
+p2.load_state_dict(
+    torch.load("../tensorboard/pokemon_critic_lr_1e2_mar2/observationsmodel/agent2_4950.pth"))
+q.load_state_dict(
+    torch.load("../tensorboard/pokemon_critic_lr_1e2_mar2/observationsmodel/val_4950.pth"))
+
+
 # initialize CoPG
 optim_q = torch.optim.Adam(q.parameters(), lr=1e-4)
 
